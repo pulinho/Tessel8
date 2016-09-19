e0f44d25ab33a18faaba4acbfa76a617a3ac5fd9
Vasek Milata <ja@pule.cz>
2016-09-20 00:05:46 +0200

    Line.getDistance() refactor

--- app/src/main/java/cz/pule/tessel8/geometry/Line.java
+++ app/src/main/java/cz/pule/tessel8/geometry/Line.java
@@ -1,5 +1,7 @@
 package cz.pule.tessel8.geometry;
 
+import android.graphics.Matrix;
+
 /**
  * Created by pule on 7. 9. 2016.
  */
@@ -22,35 +24,37 @@ public class Line {
         return p2;
     }
 
+    /**
+     * Returns the closest distance to line from specified coordinates.
+     *
+     * @param x - coordinate x from which the distance to line is computed
+     * @param y - coordinate y
+     * @return computed distance
+     */
     public float getDistance(float x, float y){
 
-        float lineDX = p2.getX() - p1.getX();
-        float lineDY = p2.getY() - p1.getY();
+        float lineLength = this.getLength();
 
-        float lineAngle = (float) -Math.atan(lineDY/lineDX);
+        Matrix m = new Matrix();
 
-        // Coordinate X2 of line, which was translated and rotated so that X1=0, Y1=0 and Y2=0
-        float transformedP2X = (float)(lineDX * Math.cos(lineAngle) - lineDY * Math.sin(lineAngle));
+        // next three lines get the transformation matrix m so that
+        // the transformed line's p1 = (0, 0) and p2 = (lineLength, 0)
+        float[] src = new float[]{p1.getX(), p1.getY(), p2.getX(), p2.getY()};
+        float[] dst = new float[]{0, 0, lineLength, 0};
+        m.setPolyToPoly(src, 0, dst, 0, 2);
 
-        float relativeX = x - p1.getX();
-        float relativeY = y - p1.getY();
+        //now we apply the matrix on point (x, y)
+        float[] coords = new float[]{x, y};
+        m.mapPoints(coords);
 
-        // X and Y transformed accordingly to line
-        float transformedX = (float)(relativeX * Math.cos(lineAngle) - relativeY * Math.sin(lineAngle));
-        float transformedY = (float)(relativeX * Math.sin(lineAngle) + relativeY * Math.cos(lineAngle));
+        //now we can easily measure the point's closest distance from the line
+        if(coords[0] >= 0 && coords[0] <= lineLength)
+            return Math.abs(coords[1]);
+        if(coords[0] < 0)
+            return (float) Math.sqrt(coords[0]*coords[0] + coords[1]*coords[1]);
 
-        if(transformedP2X < 0){
-            transformedP2X *= -1;
-            transformedX *= -1;
-        }
-
-        if(transformedX >= 0 && transformedX <= transformedP2X)
-            return Math.abs(transformedY);
-        if(transformedX < 0)
-            return (float) Math.sqrt(transformedX*transformedX + transformedY*transformedY);
-
-        float deltaX = transformedX - transformedP2X;
-        return (float) Math.sqrt(deltaX*deltaX + transformedY*transformedY);
+        float dX = coords[0] - lineLength;
+        return (float) Math.sqrt(dX*dX + coords[1]*coords[1]);
     }
 
     public Point splitAndPropagate(float x, float y){
@@ -69,6 +73,14 @@ public class Line {
         this.p2 = p;
 
         return newLine;
+    }
+
+    public float getLength(){
+
+        float lineDX = p2.getX() - p1.getX();
+        float lineDY = p2.getY() - p1.getY();
+
+        return (float) Math.sqrt(lineDX*lineDX + lineDY*lineDY);
     }
 
     public void setOnLineSplitListener(OnLineSplitListener onLineSplitListener){
